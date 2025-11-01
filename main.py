3bf3c5e5e96ee771a78cde4ce2f9d421cc1367a4
P3nguinKn1ght <zachary.rapp@gmail.com>
2025-11-01 11:13:05 -0400

    Setup client response model

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
 def main():
-    print("Hello from aiagent!")
+    # Loads my enviorment from my .env file using dotenv and reads my API key
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    # Creates my client object
+    client = genai.Client(api_key=api_key)
+
+    # Creates my response object in the client/response modle setup
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("**********************************************************************")
+    print("AI Resposne: \n")
+    print(response.text)
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
